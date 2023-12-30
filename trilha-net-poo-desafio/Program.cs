031390a34b09c5b1bd36aad3a3d6d71a07b24b62
Joao Vitor Coutinho <100632972+doubojv@users.noreply.github.com>
2023-12-30 16:44:22 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "12345678", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "45678", modelo: "Modelo 2", imei: "22222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
